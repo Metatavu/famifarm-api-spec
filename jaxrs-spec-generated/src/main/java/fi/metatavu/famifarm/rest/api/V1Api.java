40b9bde46c63a2b1ef599afb936b85d07f2f4e6f
Ville Koivukangas <ville.koivukangas12@gmail.com>
2019-02-18 09:51:36 +0200

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/metatavu/famifarm/rest/api/V1Api.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/famifarm/rest/api/V1Api.java
@@ -23,7 +23,7 @@ import javax.validation.Valid;
 
 @Path("/v1")
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJAXRSSpecServerCodegen", date = "2019-02-18T07:36:49.833+02:00[Europe/Helsinki]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJAXRSSpecServerCodegen", date = "2019-02-18T09:51:24.304+02:00[Europe/Helsinki]")
 public interface V1Api {
 
     @POST
@@ -394,10 +394,10 @@ public interface V1Api {
  @Parameter(description = "How many items to return at one time")  Integer maxResult
 ,  @QueryParam("createdBefore") 
 
- @Parameter(description = "Created before time")  OffsetDateTime createdBefore
+ @Parameter(description = "Created before time")  String createdBefore
 ,  @QueryParam("createdAfter") 
 
- @Parameter(description = "Created after time")  OffsetDateTime createdAfter
+ @Parameter(description = "Created after time")  String createdAfter
 );
     @GET
     @Path("/events")
