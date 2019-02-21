5ff44c2ed39485d95de34087675efceb1e63faa6
Antti Leppä <antti.leppa@metatavu.fi>
2019-02-21 09:47:59 +0200

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/metatavu/famifarm/rest/api/V1Api.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/famifarm/rest/api/V1Api.java
@@ -23,7 +23,7 @@ import javax.validation.Valid;
 
 @Path("/v1")
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJAXRSSpecServerCodegen", date = "2019-02-21T09:18:19.569+02:00[Europe/Helsinki]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJAXRSSpecServerCodegen", date = "2019-02-21T09:47:53.462+02:00[Europe/Helsinki]")
 public interface V1Api {
 
     @POST
