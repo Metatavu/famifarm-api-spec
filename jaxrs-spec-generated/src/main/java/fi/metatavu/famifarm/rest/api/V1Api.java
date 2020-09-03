b2589f231329260701ef12f36d88ab7b00667a35
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2020-09-03 16:12:51 +0300

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/metatavu/famifarm/rest/api/V1Api.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/famifarm/rest/api/V1Api.java
@@ -23,7 +23,7 @@ import javax.validation.Valid;
 
 @Path("/v1")
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJAXRSSpecServerCodegen", date = "2020-09-03T15:57:53.432+03:00[Europe/Helsinki]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJAXRSSpecServerCodegen", date = "2020-09-03T16:12:33.343+03:00[Europe/Helsinki]")
 public interface V1Api {
 
     @POST
